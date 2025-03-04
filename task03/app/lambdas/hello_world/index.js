8a600d9241bfc1e6939b3befdb874283088d9c89
Ayushi sarkar <ayushi_sarkar@epam.com>
2025-03-04 20:14:02 +0530

    task03-api_gateway

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,16 +1,9 @@
-//exports.handler = async (event) => {
-//    // TODO implement
-//    const response = {
-//        statusCode: 200,
-//        body: JSON.stringify('Hello from Lambda!'),
-//    };
-//    return response;
-//};
+
 
 exports.handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello, world!" }),
+        body: JSON.stringify({ message: "Hello from Lambda" }),
     };
 };
 
