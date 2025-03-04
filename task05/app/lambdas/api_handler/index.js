39ba1f4f094e9bc40ca46c44dbd01a3204b62a21
Ayushi sarkar <ayushi_sarkar@epam.com>
2025-03-05 04:50:08 +0530

    doit

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -2,13 +2,21 @@ const AWS = require("aws-sdk");
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const { v4: uuidv4 } = require("uuid");
 
-const TABLE_NAME = "cmtr-78532faa-Events-vrkf";
+const TABLE_NAME = "Events";
 
 exports.handler = async (event) => {
     try {
         console.log("Received event:", JSON.stringify(event));
 
+        if (!event.body) {
+            throw new Error("Missing event body");
+        }
+
         const requestBody = JSON.parse(event.body);
+        if (!requestBody.title || !requestBody.description) {
+            throw new Error("Missing required fields: title and/or description");
+        }
+
         const newEvent = {
             id: uuidv4(),
             principalId: 10, // Assuming a fixed value, you can change this as needed
@@ -21,6 +29,8 @@ exports.handler = async (event) => {
             TableName: TABLE_NAME,
             Item: newEvent,
         }).promise();
+
+        console.log("Event saved successfully:", JSON.stringify(newEvent));
 
         return {
             statusCode: 201,
